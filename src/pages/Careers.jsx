30c8f43c5c26790621ccf432bc8a054430047ecb
Ann1508 <anelis.ivannikova2017@gmail.com>
2025-06-09 14:10:20 +0300

    Update Date Tree

---
+++ src/pages/Careers.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
+
+const Careers = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Careers Page</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Careers;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Layout/Header';
+import Footer from '../components/Layout/Footer';
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Contact Page</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
